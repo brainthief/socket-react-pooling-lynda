68573bfb4a54dc8b9cf1dc24a2bfc2a01a957482
brainthief <vytautas1983@gmail.com>
2019-09-27 21:06:10 +0300

    Starting the presentation

--- app-server.js
+++ app-server.js
@@ -5,6 +5,7 @@ var app = express();
 var connections = [];
 var title = 'Untitled Presentation';
 var audience = [];
+var speaker = {};
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
@@ -32,12 +33,22 @@ io.sockets.on('connection', function (socket) {
 	socket.on('join', function(payload) {
 		var newMember = {
 			id: this.id,
-			name: payload.name
+			name: payload.name,
+			type: 'audience'
 		};
 		this.emit('joined', newMember);
 		audience.push(newMember);
 		io.sockets.emit('audience', audience);
 		console.log("Audience Joined: %s", payload.name);
+	});
+
+	socket.on('start', function(payload) {
+		speaker.name = payload.name
+		speaker.id = this.id;
+		speaker.type = 'speaker';
+		title = payload.title;
+		this.emit('joined', speaker);
+		console.log("Presentation Started: '%s' by %s", title, speaker.name);
 	});
 
 	socket.emit('welcome', {
